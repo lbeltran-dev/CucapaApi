1927ea9cdf2744d622616cc23d74e47b989c449d
Luis Alberto Beltrán Pérez <lbeltran@Ings-MacBook-Air.local>
2022-07-13 10:53:43 -0600

    Editing the routes of the api to match the modified model names and table names of the database

--- routes/routes.js
+++ routes/routes.js
@@ -4,13 +4,13 @@ import { getProfiles, getProfile, createProfile } from '../controllers/controlle
 import { getCandidateProfiles, getCandidateProfile, createCandidateProfile } from '../controllers/controller.candidateProfile.js';
 
 //Cucapas
-router.get('/api/users', getProfiles);
-router.get('/api/users/:id', getProfile);
-router.post('/api/users', createProfile);
+router.get('/api/profiles', getProfiles);
+router.get('/api/profiles/:id', getProfile);
+router.post('/api/profiles', createProfile);
 
 //Usuarios sin verificar
-router.get('/api/possibleUsers', getCandidateProfiles);
-router.get('/api/possibleUsers/:id', getCandidateProfile);
-router.post('/api/possibleUsers', createCandidateProfile);
+router.get('/api/candidateProfiles', getCandidateProfiles);
+router.get('/api/candidateProfiles/:id', getCandidateProfile);
+router.post('/api/candidateProfiles', createCandidateProfile);
 
 export default router;
